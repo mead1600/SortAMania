b80c1462cb7994d9e6bbaab2d1f258787e5f28d8
BT_1N3_18 <mgyawu0974@bths.edu>
2018-11-30 11:24:52 -0500

    weye5wrtg

--- src/Runner.java
+++ src/Runner.java
@@ -4,6 +4,7 @@ public class Runner {
     public static void main(String[] args) {
         String[] Stringy = Team_11_SortCompetition.randomStringArr(10);
         System.out.println(Arrays.toString(Stringy));
-        Team_11_SortCompetition.StringSortandSearch(Stringy,"sdfsdfsdfsdfsdfadf");
+        System.out.println(Team_11_SortCompetition.StringSortandSearch(Stringy,"sdfsd"));
+        System.out.println(Arrays.toString(Stringy));
     }
 }
--- src/Team_11_SortCompetition.java
+++ src/Team_11_SortCompetition.java
@@ -1,9 +1,8 @@
 public class Team_11_SortCompetition {
 
     public static int StringSortandSearch(String[] arr, String given){
-        String[] temp = new String[0];
         int index = 0;
-        StringMergeSort(arr, 0, arr.length/2, arr.length-1, temp);
+        StringMergeSort(arr);
         for(int i = 0; i < arr.length;i++){
             if(arr[i].equals(given)){
                 index = i;
@@ -14,16 +13,17 @@ public class Team_11_SortCompetition {
         return index;
     }
 
-    public static void StringMergeSort(String[] arr, int left, int mid,int right, String[] temp){
+    public static void StringMerge(String[] arr, int left, int mid,int right, String[] temp){
         int i = left;
         int j = mid + 1;
         int k = left;
-        while( i <= mid && j <= right-1){
-            if(arr[i].compareTo(arr[j]) > 0){
+        while( i <= mid && j <= right){
+            if(arr[i].compareTo(arr[j]) < 0){
                 temp[k] = arr[i];
                 i++;
             }else{
                 temp[k] = arr[j];
+                j++;
             }
             k++;
         }
@@ -32,17 +32,32 @@ public class Team_11_SortCompetition {
             i++;
             k++;
         }
-        while(j <= mid){
+        while(j <= right){
             temp[k] = arr[j];
             j++;
             k++;
         }
-        for(int a = left; a < right; a++){
-            arr[a] = temp[a];
+        for(k = left; k <= right; k++){
+            arr[k] = temp[k];
         }
     }
 
-    public void mergeSort(int[] arr, int left, int mid,int right, int[] temp){
+    public static void StringMergeSortHelper(String[] elements, int from, int to, String[] temp){
+        if(from < to){
+            int middle = (from + to) / 2;
+            StringMergeSortHelper(elements, from, middle, temp);
+            StringMergeSortHelper(elements,middle+1,to,temp);
+            StringMerge(elements, from, middle, to, temp);
+        }
+    }
+
+    public static void StringMergeSort(String[] elements){
+        int n = elements.length;
+        String[] temp = new String[n];
+        StringMergeSortHelper(elements,0,n-1,temp);
+    }
+
+    public void merge(int[] arr, int left, int mid,int right, int[] temp){
         int i = left;
         int j = mid + 1;
         int k = left;
